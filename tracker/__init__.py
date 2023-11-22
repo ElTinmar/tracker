f90cff9bfd6e90cd5775b8726d9a93233a347a93
ElTinmar <martin.privat@hotmail.fr>
2023-11-22 13:54:25 +0100

    update

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -1,16 +1,18 @@
-__all__ = ["animal", "assignment", "body", "eyes", "tail", "tracker",
-           "animal_widget", "assignment_widget", "body_widget", "eyes_widget", "tail_widget", "tracker_widget"]
+__all__ = ["animal", "assignment", "body", "eyes", "tail", "multifish",
+           "animal_widget", "assignment_widget", "body_widget", "eyes_widget", "tail_widget", "multifish_widget"]
 
 from .animal import *
 from .assignment import *
 from .body import *
 from .eyes import *
 from .tail import *
-from .tracker import *
+from .multifish import *
+from .singlefish import *
 
 from .animal_widget import *
 from .assignment_widget import *
 from .body_widget import *
 from .eyes_widget import *
 from .tail_widget import *
-from .tracker_widget import *
+from .multifish_widget import *
+from .singlefish_widget import *
