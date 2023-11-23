e642daf0240be630c414c883e7d9f49970e49abf
ElTinMar <martin.privat@hotmail.fr>
2023-11-23 11:15:35 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='tracker',
     author='Martin Privat',
-    version='0.1',
+    version='0.1.1',
     packages=['tracker','tracker.tests'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='tracking zebrafish larvae',
