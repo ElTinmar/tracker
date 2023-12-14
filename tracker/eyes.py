c217d82c57cc28aff861a0173a96a1aed8d4795b
ElTinMar <martin.privat@hotmail.fr>
2023-12-14 16:00:26 +0100

    add GPU version

--- tracker/eyes.py
+++ tracker/eyes.py
@@ -14,6 +14,12 @@ from image_tools import (
 from geometry import ellipse_direction, angle_between_vectors, to_homogeneous, from_homogeneous
 from tracker import Tracker, TrackingOverlay
 
+try:
+    import cupy as cp
+    from cupy.typing import NDArray as CuNDArray
+except:
+    print('No GPU available, cupy not imported')
+
 @dataclass
 class EyesTrackerParamTracking:
     pix_per_mm: float = 40.0
@@ -165,6 +171,34 @@ def find_eyes_and_swimbladder(
         if len(props) == 3:
             found_eyes_and_sb = True
             mask = bwareafilter(
+                mask, 
+                min_size = eye_size_lo_px, 
+                max_size = eye_size_hi_px
+            )
+            break
+
+    return (found_eyes_and_sb, props, mask)
+
+def find_eyes_and_swimbladder_GPU(
+        image: CuNDArray, 
+        eye_dyntresh_res: int, 
+        eye_size_lo_px: float, 
+        eye_size_hi_px: float
+    ) -> Tuple:
+    
+    # OPTIM this is slow
+    thresholds = np.linspace(1/eye_dyntresh_res,1,eye_dyntresh_res)
+    found_eyes_and_sb = False
+    for t in thresholds:
+        mask = 1.0*(image >= t)
+        props = bwareafilter_props_GPU(
+            mask, 
+            min_size = eye_size_lo_px, 
+            max_size = eye_size_hi_px
+        )
+        if len(props) == 3:
+            found_eyes_and_sb = True
+            mask = bwareafilter_GPU(
                 mask, 
                 min_size = eye_size_lo_px, 
                 max_size = eye_size_hi_px
@@ -357,8 +391,86 @@ class EyesOverlay(TrackingOverlay):
 
 class EyesTrackerGPU(Tracker):
 
+    def __init__(
+            self, 
+            tracking_param: EyesTrackerParamTracking, 
+        ) -> None:
+
+        self.tracking_param = tracking_param
+
     def track(
-            image: NDArray, 
+            self,
+            image: CuNDArray, 
             centroid: Optional[NDArray],
         ) -> Optional[EyesTracking]:
-        '''TODO'''
+
+        if (image is None) or (image.size == 0) or (centroid is None):
+            return None
+
+        image_gpumat = cupy_array_to_GpuMat(image)
+
+        if self.tracking_param.resize != 1:
+            image_gpumat = cv2.resize(
+                image_gpumat, 
+                None, 
+                None,
+                self.tracking_param.resize,
+                self.tracking_param.resize,
+                cv2.INTER_NEAREST
+            )
+
+        image = GpuMat_to_cupy_array(image_gpumat)
+
+        left_eye = None
+        right_eye = None
+        new_heading = None
+
+        # crop image
+        w, h = self.tracking_param.crop_dimension_px
+        offset = np.array((-w//2, -h//2+self.tracking_param.crop_offset_px), dtype=np.int32)
+        left, bottom = (centroid * self.tracking_param.resize).astype(np.int32) + offset 
+        right, top = left+w, bottom+h 
+
+        image_crop = image[bottom:top, left:right]
+        if image_crop.size == 0:
+            return None
+
+        # tune image contrast and gamma
+        image_crop = enhance_GPU(
+            image_crop,
+            self.tracking_param.eye_contrast,
+            self.tracking_param.eye_gamma,
+            self.tracking_param.eye_brightness,
+            self.tracking_param.blur_sz_px,
+            self.tracking_param.median_filter_sz_px
+        )
+
+        # sweep threshold to obtain 3 connected component within size range (include SB)
+        found_eyes_and_sb, props, mask = find_eyes_and_swimbladder_GPU(
+            image_crop, 
+            self.tracking_param.eye_dyntresh_res, 
+            self.tracking_param.eye_size_lo_px, 
+            self.tracking_param.eye_size_hi_px
+        )
+        
+        if found_eyes_and_sb: 
+            # identify left eye, right eye and swimbladder
+            blob_centroids = np.array([blob.centroid for blob in props])
+            sb_idx, left_idx, right_idx = assign_features(blob_centroids)
+
+            # compute eye orientation
+            left_eye = get_eye_prop(props[left_idx], offset, self.tracking_param.resize)
+            right_eye = get_eye_prop(props[right_idx], offset, self.tracking_param.resize)
+            #new_heading = (props[left_idx].centroid + props[right_idx].centroid)/2 - props[sb_idx].centroid
+            #new_heading = new_heading / np.linalg.norm(new_heading)
+
+        res = EyesTracking(
+            centroid = centroid,
+            offset = offset,
+            left_eye = left_eye,
+            right_eye = right_eye,
+            mask = im2uint8(mask.get()),
+            image = im2uint8(image_crop.get())
+        )
+
+        return res
