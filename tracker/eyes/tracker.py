17b6461974aa532bc1fb03d89590ee238314ef86
ElTinmar <martin.privat@hotmail.fr>
2023-12-22 18:06:42 +0100

    update

--- tracker/eyes/tracker.py
+++ tracker/eyes/tracker.py
@@ -1,75 +1,10 @@
 import cv2
-from scipy.spatial.distance import pdist
 import numpy as np
-from numpy.typing import NDArray, ArrayLike
-from typing import Tuple, Dict, Optional
-from image_tools import bwareafilter_props, bwareafilter, enhance, im2uint8
-from geometry import ellipse_direction, angle_between_vectors, to_homogeneous, from_homogeneous
+from numpy.typing import NDArray
+from typing import Optional
+from image_tools import enhance, im2uint8
 from .core import EyesTracker, EyesTracking
-
-def get_eye_prop(blob, offset: NDArray, resize: float) -> Dict:
-
-    # fish must be vertical head up
-    heading = np.array([0, 1], dtype=np.float32)
-
-    eye_dir = ellipse_direction(blob.inertia_tensor, heading)
-    eye_angle = angle_between_vectors(eye_dir, heading)
-    # (row,col) to (x,y) coordinates 
-    y, x = blob.centroid 
-    eye_centroid = np.array([x, y], dtype = np.float32) + offset
-    return {'direction': eye_dir, 'angle': eye_angle, 'centroid': eye_centroid/resize}
-
-
-def assign_features(blob_centroids: ArrayLike) -> Tuple[int, int, int]:
-    """From Duncan, returns indices of swimbladder, left eye and right eye"""
-    
-    centroids = np.asarray(blob_centroids)
-    
-    # find swimbladder
-    distances = pdist(centroids)
-    sb_idx = 2 - np.argmin(distances)
-
-    # find eyes
-    eye_idxs = [i for i in range(3) if i != sb_idx]
-    
-    # Getting left and right eyes
-    # NOTE: numpy automatically adds 0 to 3rd dimension when 
-    # computing cross-product if input arrays are 2D.
-    eye_vectors = centroids[eye_idxs] - centroids[sb_idx]
-    cross_product = np.cross(*eye_vectors)
-    if cross_product < 0:
-        eye_idxs = eye_idxs[::-1]
-    left_idx, right_idx = eye_idxs
-
-    return sb_idx, left_idx, right_idx
-
-def find_eyes_and_swimbladder(
-        image: NDArray, 
-        eye_dyntresh_res: int, 
-        eye_size_lo_px: float, 
-        eye_size_hi_px: float
-    ) -> Tuple:
-    
-    # OPTIM this is slow
-    thresholds = np.linspace(1/eye_dyntresh_res,1,eye_dyntresh_res)
-    found_eyes_and_sb = False
-    for t in thresholds:
-        mask = 1.0*(image >= t)
-        props = bwareafilter_props(
-            mask, 
-            min_size = eye_size_lo_px, 
-            max_size = eye_size_hi_px
-        )
-        if len(props) == 3:
-            found_eyes_and_sb = True
-            mask = bwareafilter(
-                mask, 
-                min_size = eye_size_lo_px, 
-                max_size = eye_size_hi_px
-            )
-            break
-
-    return (found_eyes_and_sb, props, mask)
+from .utils import get_eye_prop, find_eyes_and_swimbladder, assign_features
 
 class EyesTracker_CPU(EyesTracker):
 
