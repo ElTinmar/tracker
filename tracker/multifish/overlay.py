a4dcb4c8c7be5027864fbd787e72b1d61168651a
ElTinmar <martin.privat@hotmail.fr>
2024-04-26 14:43:38 +0200

    update

--- tracker/multifish/overlay.py
+++ tracker/multifish/overlay.py
@@ -31,7 +31,8 @@ class MultiFishOverlay_opencv(MultiFishOverlay):
             for idx, id in zip(tracking.animals.indices, tracking.animals.identities):
 
                 # transformation matrix from coord system 1. to coord system 2., just a translation  
-                T_bbox_to_image = Affine2DTransform.translation(tracking.animals.centroids[idx,:])
+                tx, ty = tracking.animals.centroids[idx,:]
+                T_bbox_to_image = Affine2DTransform.translation(tx,ty)
 
                 if (self.body is not None) and (tracking.body[id] is not None) and (tracking.body[id].centroid is not None):
 
