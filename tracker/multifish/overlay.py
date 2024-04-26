cccdb40e28cd1881789f1841960991e8be0b0aeb
ElTinmar <martin.privat@hotmail.fr>
2024-04-26 17:36:47 +0200

    what am I even doing

--- tracker/multifish/overlay.py
+++ tracker/multifish/overlay.py
@@ -30,11 +30,11 @@ class MultiFishOverlay_opencv(MultiFishOverlay):
             # loop over animals
             for idx, id in zip(tracking.animals.indices, tracking.animals.identities):
 
-                # transformation matrix from coord system 1. to coord system 2., just a translation  
-                tx, ty = tracking.animals.centroids[idx,:] - np.asarray(tracking.body.image_fullres.shape)//2 # dirty fix?
-                T_bbox_to_image = Affine2DTransform.translation(tx,ty)
-
                 if (self.body is not None) and (tracking.body[id] is not None) and (tracking.body[id].centroid is not None):
+
+                    # transformation matrix from coord system 1. to coord system 2., just a translation  
+                    tx, ty = tracking.animals.centroids[idx,:] - np.asarray(tracking.body[id].image_fullres.shape)//2 # dirty fix?
+                    T_bbox_to_image = Affine2DTransform.translation(tx,ty)
 
                     # overlay body, coord. system 2.
                     overlay = self.body.overlay(
