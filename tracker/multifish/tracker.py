00f7a89e1bd697e56f440eb7ce8ecf8ad78494dc
ElTinmar <martin.privat@hotmail.fr>
2025-01-30 14:24:57 +0100

    handle none

--- tracker/multifish/tracker.py
+++ tracker/multifish/tracker.py
@@ -9,42 +9,44 @@ class MultiFishTracker_CPU(MultiFishTracker):
     def track(self, image: NDArray, centroid: Optional[NDArray] = None) -> Optional[NDArray]:
 
         if (image is None) or (image.size == 0):
-            return None
+            return 
 
         # get animal centroids (only crude location is necessary)
         animals = self.tracking_param.animal.track(image)
 
+        if animals is None or animals['identities'] is None:
+            return
+        
         body = []
         eyes = []
         tail = []
 
         # loop over detected animals to get body, eyes and tail tracking
 
-        if animals['identities'] is not None:
-            for id in animals['identities']:
-                if self.tracking_param.body is not None:
+        for id in animals['identities']:
+            if self.tracking_param.body is not None:
 
-                    # get more precise centroid and orientation of the animals
-                    b = self.tracking_param.body.track(image, centroid=animals['centroids'][id,:])
-                    body.append(b)
+                # get more precise centroid and orientation of the animals
+                b = self.tracking_param.body.track(image, centroid=animals['centroids'][id,:])
+                body.append(b)
+                
+                # if body was found, track eyes and tail
+                if (b is not None) and (b['centroid'] is not None):
                     
-                    # if body was found, track eyes and tail
-                    if (b is not None) and (b['centroid'] is not None):
-                        
-                        # rotate the animal so that it's vertical head up
-                        image_rot, centroid_rot = imrotate(
-                            b['image_fullres'], 
-                            b['centroid'][0], b['centroid'][1], 
-                            np.rad2deg(b['angle_rad'])
-                        )
+                    # rotate the animal so that it's vertical head up
+                    image_rot, centroid_rot = imrotate(
+                        b['image_fullres'], 
+                        b['centroid'][0], b['centroid'][1], 
+                        np.rad2deg(b['angle_rad'])
+                    )
 
-                        # track eyes
-                        if self.tracking_param.eyes is not None:
-                            eyes.append(self.tracking_param.eyes.track(image_rot, centroid=centroid_rot))
+                    # track eyes
+                    if self.tracking_param.eyes is not None:
+                        eyes.append(self.tracking_param.eyes.track(image_rot, centroid=centroid_rot))
 
-                        # track tail
-                        if self.tracking_param.tail is not None:
-                            tail.append(self.tracking_param.tail.track(image_rot, centroid=centroid_rot))
+                    # track tail
+                    if self.tracking_param.tail is not None:
+                        tail.append(self.tracking_param.tail.track(image_rot, centroid=centroid_rot))
 
         # compute additional features based on tracking
         if self.tracking_param.accumulator is not None:
