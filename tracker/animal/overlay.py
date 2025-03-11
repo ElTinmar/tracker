48bb262a6ef9807f7bc2ac8a259c48831071dc56
ElTinmar <martin.privat@hotmail.fr>
2025-03-11 10:30:12 +0100

    update overlay

--- tracker/animal/overlay.py
+++ tracker/animal/overlay.py
@@ -6,47 +6,85 @@ import cv2
 from typing import Optional
 from .core import AnimalOverlay
 
-# TODO maybe one function for input / cropped / resized space?
-# no need to pass image as input, use image in tracking
-
 class AnimalOverlay_opencv(AnimalOverlay):
 
-    def overlay(
+    def overlay_global(
             self,
             image: NDArray, 
-            tracking: Optional[NDArray], 
+            tracking: Optional[NDArray],
             transformation_matrix: NDArray = Affine2DTransform.identity()
         ) -> Optional[NDArray]:
 
-        if tracking is not None:
-
-            overlay = im2rgb(im2uint8(image))
-            original = overlay.copy()        
-
-            for centroid in tracking['centroids_global']:
-
-                # draw centroid
-                x,y = transform2d(transformation_matrix, centroid)
-                
-                overlay = cv2.circle(
-                    overlay,
-                    (int(centroid[0]),int(centroid[1])), 
-                    self.overlay_param.radius_px, 
-                    self.overlay_param.centroid_color_BGR, 
-                    self.overlay_param.centroid_thickness
-                )
-                
-                # show ID
-                cv2.putText(
-                    overlay, 
-                    str(id), (int(x+self.overlay_param.label_offset), int(y-self.overlay_param.label_offset)), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 1.5, 
-                    self.overlay_param.id_str_color_BGR, 
-                    2, 
-                    cv2.LINE_AA
-                )
+        if tracking is None:
+            return None
             
-            overlay = cv2.addWeighted(overlay, self.overlay_param.alpha, original, 1 - self.overlay_param.alpha, 0)
+        return self._overlay_centroids(
+            centroids = tracking['centroids_global'],
+            image = image,
+            transformation_matrix = transformation_matrix
+        )
 
-            return overlay
+    def overlay_cropped(self, tracking: Optional[NDArray]) -> Optional[NDArray]:
+
+        if tracking is None:
+            return None
         
+        S = Affine2DTransform.scaling(
+            tracking['downsample_ratio'],
+            tracking['downsample_ratio']
+        )
+
+        return self._overlay_centroids(
+            centroids = tracking['centroids_cropped'],
+            image = tracking['image_downsampled'],
+            transformation_matrix = S
+        )
+
+    def overlay_resized(self, tracking: Optional[NDArray]) -> Optional[NDArray]:
+        
+        if tracking is None:
+            return None
+        
+        return self._overlay_centroids(
+            centroids = tracking['centroids_resized'],
+            image = tracking['image_processed']
+        )
+
+    def _overlay_centroids(
+            self,
+            centroids: NDArray,
+            image: NDArray, 
+            transformation_matrix: NDArray = Affine2DTransform.identity()
+        ) -> NDArray:
+
+
+        overlay = im2rgb(im2uint8(image))
+        original = overlay.copy()        
+
+        for idx, centroid in enumerate(centroids):
+
+            # draw centroid
+            x,y = transform2d(transformation_matrix, centroid)
+            
+            overlay = cv2.circle(
+                overlay,
+                (int(x),int(y)), 
+                self.overlay_param.radius_px, 
+                self.overlay_param.centroid_color_BGR, 
+                self.overlay_param.centroid_thickness
+            )
+            
+            # show ID
+            cv2.putText(
+                overlay, 
+                str(idx), (int(x+self.overlay_param.label_offset), int(y-self.overlay_param.label_offset)), 
+                cv2.FONT_HERSHEY_SIMPLEX, 1.5, 
+                self.overlay_param.id_str_color_BGR, 
+                2, 
+                cv2.LINE_AA
+            )
+        
+        overlay = cv2.addWeighted(overlay, self.overlay_param.alpha, original, 1 - self.overlay_param.alpha, 0)
+
+        return overlay
+    
