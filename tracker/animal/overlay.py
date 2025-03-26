1e0db355ebb7b859b3c92a4844a8a3bb721b038e
ElTinmar <martin.privat@hotmail.fr>
2025-03-26 15:23:31 +0100

    remove text label

--- tracker/animal/overlay.py
+++ tracker/animal/overlay.py
@@ -62,14 +62,13 @@ class AnimalOverlay_opencv(AnimalOverlay):
         overlay = im2rgb(im2uint8(image))
         original = overlay.copy()        
 
-        for idx, centroid in enumerate(centroids):
+        for centroid in centroids:
 
             # draw centroid
             x,y = transformation.transform_points(centroid).ravel()
 
             pix_per_mm_input =  pix_per_mm * transformation.scale_factor
             radius_px = max(1,int(self.overlay_param.radius_mm * pix_per_mm_input))
-            font_height_px = max(1, int(self.overlay_param.id_str_height_mm * pix_per_mm_input))
             
             overlay = cv2.circle(
                 overlay,
@@ -77,22 +76,6 @@ class AnimalOverlay_opencv(AnimalOverlay):
                 radius_px, 
                 self.overlay_param.centroid_color_BGR, 
                 self.overlay_param.centroid_thickness
-            )
-            
-            font_size = cv2.getFontScaleFromHeight(
-                self.overlay_param.font,
-                font_height_px, 
-                self.overlay_param.font_thickness
-            )
-
-            # show ID
-            cv2.putText(
-                overlay, 
-                str(idx), (int(x+self.overlay_param.label_offset), int(y-self.overlay_param.label_offset)), 
-                self.overlay_param.font, font_size, 
-                self.overlay_param.id_str_color_BGR, 
-                self.overlay_param.font_thickness, 
-                self.overlay_param.font_line_type
             )
         
         overlay = cv2.addWeighted(
