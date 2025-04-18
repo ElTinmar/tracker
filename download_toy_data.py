637203b9deb6264a26527cd7a9d8891f1ea93bbd
ElTinmar <martin.privat@hotmail.fr>
2025-04-18 19:11:52 +0200

    update

--- download_toy_data.py
+++ download_toy_data.py
@@ -5,11 +5,24 @@ import os
 FOLDER_URL = 'https://owncloud.gwdg.de/index.php/s/3I1wGOdmh8W2XaZ/download'
 DESTINATION = 'toy_data.zip'
 
-print('Downloading toy data...')
-urlretrieve(FOLDER_URL, DESTINATION)
-print(f'Extracting {DESTINATION}...')
-with zipfile.ZipFile(DESTINATION, 'r') as zip_ref:
-    zip_ref.extractall()
-print("Extraction complete.")
-print(f'Removing {DESTINATION}...')
-os.remove(DESTINATION)
+def download() -> None:
+    print('Downloading toy data...')
+    urlretrieve(FOLDER_URL, DESTINATION)
+
+def extract() -> None:
+    print(f'Extracting {DESTINATION}...')
+    with zipfile.ZipFile(DESTINATION, 'r') as zip_ref:
+        zip_ref.extractall()
+    print("Extraction complete.")
+
+def cleanup() -> None:
+    print(f'Removing {DESTINATION}...')
+    os.remove(DESTINATION)
+
+if __name__ == '__main__':
+    try:
+        download()
+        extract()
+    finally:
+        cleanup()
+
