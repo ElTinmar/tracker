d3f397a3b9d805259d920397e8c8a1a9f3a068cf
ElTinmar <martin.privat@hotmail.fr>
2025-04-18 18:54:58 +0200

    download data

---
+++ download_toy_data.py
@@ -0,0 +1,7 @@
+from urllib.request import urlretrieve
+
+FOLDER_URL = 'https://owncloud.gwdg.de/index.php/s/3I1wGOdmh8W2XaZ'
+DESTINATION = 'toy_data'
+
+print('Downloading toy data...')
+urlretrieve(FOLDER_URL, DESTINATION)
