095074b75735fcc76efec71fb60ad0dbb9055c1a
ElTinmar <martin.privat@hotmail.fr>
2025-04-18 19:06:11 +0200

    download example data

--- download_toy_data.py
+++ download_toy_data.py
@@ -1,7 +1,14 @@
 from urllib.request import urlretrieve
+import zipfile
+import os
 
 FOLDER_URL = 'https://owncloud.gwdg.de/index.php/s/3I1wGOdmh8W2XaZ/download'
-DESTINATION = 'toy_data'
+DESTINATION = 'toy_data.zip'
 
 print('Downloading toy data...')
 urlretrieve(FOLDER_URL, DESTINATION)
+print(f'Extracting {DESTINATION}...')
+with zipfile.ZipFile(DESTINATION, 'r') as zip_ref:
+    zip_ref.extractall()
+print(f'remove {DESTINATION}...')
+os.remove(DESTINATION)
