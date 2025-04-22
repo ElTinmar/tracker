c449f3f97254c713a0ef2426058bd5a5ae7f4dcb
ElTinmar <martin.privat@hotmail.fr>
2025-04-22 10:43:23 +0200

    split into functions

--- tracker/body/tracker.py
+++ tracker/body/tracker.py
@@ -1,17 +1,107 @@
 from image_tools import  bwareafilter_props_cv2
 import numpy as np
 from numpy.typing import NDArray
-from typing import Optional
+from typing import Optional, Tuple
 from .core import BodyTracker
 from .utils import get_orientation, get_best_centroid_index
-from tracker.prepare_image import preprocess_image
+from tracker.prepare_image import preprocess_image, Preprocessing
 from geometry import SimilarityTransform2D, angdiff, normalize_angle
 import cv2
 from filterpy.common import kinematic_kf
 from collections import deque
+from dataclasses import dataclass
+
+@dataclass
+class Tracking:
+    centroid_resized: NDArray = np.zeros((2,), np.float32)
+    centroid_cropped: NDArray = np.zeros((2,), np.float32)
+    centroid_input: NDArray = np.zeros((2,), np.float32)
+    centroid_global: NDArray = np.zeros((2,), np.float32)
+    body_axes: NDArray = np.zeros((2,2), np.float32)
+    body_axes_global: NDArray = np.zeros((2,2), np.float32)
+    angle_rad: float = 0.0
+    angle_rad_global: float = 0.0
+
+@dataclass
+class Resolution:
+    pix_per_mm_global: float = 0.0
+    pix_per_mm_input: float = 0.0
+    pix_per_mm_cropped: float = 0.0
+    pix_per_mm_resized: float = 0.0
 
 class BodyTracker_CPU(BodyTracker):
+
+    def preprocess(
+        self,
+        image: NDArray, 
+        centroid_in: Optional[NDArray] = None, # centroids in global space
+        ) -> Optional[Tuple[Preprocessing, NDArray]]:
         
+        preproc = preprocess_image(image, centroid_in, self.tracking_param)
+
+        if preproc is None:
+            return self.tracking_param.failed
+            
+        centroid_in_cropped = preproc.T_input_to_cropped.transform_points(centroid_in).squeeze()
+        centroid_in_resized = preproc.T_cropped_to_resized.transform_points(centroid_in_cropped).squeeze()
+
+        return preproc, centroid_in_resized
+
+    def track_resized(
+            self,
+            preproc: Preprocessing, 
+            centroid_in_resized: Optional[NDArray] = None, # centroids in global space
+        ) -> Optional[Tuple[Tracking, NDArray]]:
+
+        mask = cv2.compare(preproc.image_processed, self.tracking_param.intensity, cv2.CMP_GT)
+        props = bwareafilter_props_cv2(
+            mask, 
+            min_size = self.tracking_param.min_size_px,
+            max_size = self.tracking_param.max_size_px, 
+            min_length = self.tracking_param.min_length_px,
+            max_length = self.tracking_param.max_length_px,
+            min_width = self.tracking_param.min_width_px,
+            max_width = self.tracking_param.max_width_px
+        )
+
+        if not props:
+            return None
+        
+        tracking = Tracking()
+        
+        centroids_resized = np.array([[blob.centroid[1], blob.centroid[0]] for blob in props]) #(row, col) to (x,y)
+        index = get_best_centroid_index(centroids_resized, centroid_in_resized)
+        tracking.centroid_resized = centroids_resized[index]
+        coordinates_resized = np.fliplr(props[index].coords)
+        tracking.body_axes = get_orientation(coordinates_resized)
+        if tracking.body_axes is None:
+            return self.tracking_param.failed
+        tracking.angle_rad = np.arctan2(tracking.body_axes[1,1], tracking.body_axes[0,1])
+
+        return tracking, mask
+        
+    def transform_coordinate_system(
+            self,
+            tracking: Tracking,        
+            preproc: Preprocessing,
+            T_input_to_global,
+            T_global_to_input
+        ) -> Resolution:
+        '''modifies tracking in-place with side-effect and returns resolution'''
+        
+        tracking.centroid_cropped = preproc.T_resized_to_cropped.transform_points(tracking.centroid_resized)
+        tracking.centroid_input = preproc.T_cropped_to_input.transform_points(tracking.centroid_cropped)
+        tracking.centroid_global = T_input_to_global.transform_points(tracking.centroid_input)
+        tracking.body_axes_global = T_input_to_global.transform_vectors(tracking.body_axes) 
+        tracking.angle_rad_global = np.arctan2(tracking.body_axes_global[1,1], tracking.body_axes_global[0,1])
+
+        resolution = Resolution()
+        resolution.pix_per_mm_global = self.tracking_param.pix_per_mm
+        resolution.pix_per_mm_input = resolution.pix_per_mm_global * T_global_to_input.scale_factor
+        resolution.pix_per_mm_cropped = resolution.pix_per_mm_input * preproc.T_input_to_cropped.scale_factor
+        resolution.pix_per_mm_resized = resolution.pix_per_mm_cropped * preproc.T_cropped_to_resized.scale_factor
+        return resolution
+
     def track(
             self,
             image: NDArray, 
@@ -26,80 +116,49 @@ class BodyTracker_CPU(BodyTracker):
             - (0,0) = topleft corner of the bounding box
             - scale of the full-resolution image, before resizing
         '''
-        
+
         self.tracking_param.input_image_shape = image.shape
-
         T_global_to_input = T_input_to_global.inv()
-        
         if centroid is None:
-            centroid_input = None
+            centroid_in_input = None
         else:
-            centroid_input = T_global_to_input.transform_points(centroid).squeeze()
-
-        preproc = preprocess_image(image, centroid_input, self.tracking_param)
-            
-        if preproc is None:
+            centroid_in_input = T_global_to_input.transform_points(centroid).squeeze()
+        
+        preprocessing = self.preprocess(image, centroid_in_input)
+        if preprocessing is None:
+            return self.tracking_param.failed
+        
+        preproc, centroid_in_resized = preprocessing 
+        tracking_resized = self.track_resized(preproc, centroid_in_resized)
+        if tracking_resized is None:
             return self.tracking_param.failed
 
-        mask = cv2.compare(preproc.image_processed, self.tracking_param.intensity, cv2.CMP_GT)
-        props = bwareafilter_props_cv2(
-            mask, 
-            min_size = self.tracking_param.min_size_px,
-            max_size = self.tracking_param.max_size_px, 
-            min_length = self.tracking_param.min_length_px,
-            max_length = self.tracking_param.max_length_px,
-            min_width = self.tracking_param.min_width_px,
-            max_width = self.tracking_param.max_width_px
+        tracking, mask = tracking_resized
+        resolution = self.transform_coordinate_system(
+            tracking, 
+            preproc, 
+            T_input_to_global, 
+            T_global_to_input
         )
-
-        if not props:
-            return self.tracking_param.failed
-        
-        centroids_resized = np.array([[blob.centroid[1], blob.centroid[0]] for blob in props]) #(row, col) to (x,y)
-        centroids_cropped = preproc.T_resized_to_cropped.transform_points(centroids_resized)
-        centroids_input = preproc.T_cropped_to_input.transform_points(centroids_cropped)
-        centroids_global = T_input_to_global.transform_points(centroids_input)
-
-        # get coordinates of best centroid
-        index = get_best_centroid_index(centroids_global, centroid)
-        centroid_resized = centroids_resized[index]
-        centroid_cropped = centroids_cropped[index]
-        centroid_input = centroids_input[index]
-        centroid_global = centroids_global[index]
-
-        coordinates_resized = np.fliplr(props[index].coords)
-        body_axes = get_orientation(coordinates_resized)
-        if body_axes is None:
-            return self.tracking_param.failed
-        
-        body_axes_global = T_input_to_global.transform_vectors(body_axes) 
-        
-        angle_rad = np.arctan2(body_axes[1,1], body_axes[0,1])
-        angle_rad_global = np.arctan2(body_axes_global[1,1], body_axes_global[0,1])
-
-        pix_per_mm_global = self.tracking_param.pix_per_mm
-        pix_per_mm_input = pix_per_mm_global * T_global_to_input.scale_factor
-        pix_per_mm_cropped = pix_per_mm_input * preproc.T_input_to_cropped.scale_factor
-        pix_per_mm_resized = pix_per_mm_cropped * preproc.T_cropped_to_resized.scale_factor
         
         res = np.array(
             (
                 True,
-                body_axes, 
-                body_axes_global,
-                centroid_resized,
-                centroid_cropped,
-                centroid_input,
-                centroid_global,
-                angle_rad,
-                angle_rad_global,
+                tracking.body_axes, 
+                tracking.body_axes_global,
+                tracking.centroid_resized,
+                tracking.centroid_cropped,
+                tracking.centroid_input,
+                tracking.centroid_global,
+                tracking.angle_rad,
+                tracking.angle_rad_global,
                 mask, 
                 preproc.image_processed,
                 preproc.image_cropped,
-                pix_per_mm_global,
-                pix_per_mm_input,
-                pix_per_mm_cropped,
-                pix_per_mm_resized,
+                resolution.pix_per_mm_global,
+                resolution.pix_per_mm_input,
+                resolution.pix_per_mm_cropped,
+                resolution.pix_per_mm_resized,
             ), 
             dtype=self.tracking_param.dtype
         )
