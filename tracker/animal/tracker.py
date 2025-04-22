d8dfc57fb7327b7292a5b5dad45510af688704a0
ElTinmar <martin.privat@hotmail.fr>
2025-04-22 12:28:49 +0200

    splittracking into functions

--- tracker/animal/tracker.py
+++ tracker/animal/tracker.py
@@ -4,28 +4,57 @@ from numpy.typing import NDArray
 import cv2
 from typing import Optional, Tuple
 from .core import AnimalTracker
-from tracker.prepare_image import preprocess_image
+from tracker.prepare_image import preprocess_image, Preprocessing
+from tracker.core import Resolution
 from geometry import SimilarityTransform2D
 from filterpy.common import kinematic_kf
+from dataclasses import dataclass
+
+@dataclass
+class AnimalResolution(Resolution):
+    pix_per_mm_downsampled: float = 0.0
+
+class Tracking:
+    def __init__(self, num_animals: int) -> None:
+        self.num_animals: int = num_animals
+        self.centroids_resized: NDArray = np.zeros((num_animals, 2), np.float32)
+        self.centroids_cropped: NDArray = np.zeros((num_animals, 2), np.float32)
+        self.centroids_input: NDArray = np.zeros((num_animals, 2), np.float32)
+        self.centroids_global: NDArray = np.zeros((num_animals, 2), np.float32)
 
 class AnimalTracker_CPU(AnimalTracker):
-    
-    def track(
+
+    def transform_input_centroid(
+            self, 
+            centroid, # centroid in global space
+            T_input_to_global
+        ) -> Tuple[Optional[NDArray], SimilarityTransform2D]:
+        
+        T_global_to_input = T_input_to_global.inv()
+
+        if centroid is None:
+            centroid_input = None
+        else:
+            centroid_input = T_global_to_input.transform_points(centroid).squeeze()
+
+        return centroid_input, T_global_to_input        
+
+    def preprocess(
         self,
-        image: Optional[NDArray], # image in input space
-        centroid: Optional[NDArray] = None, # centroid in global space
-        T_input_to_global: Optional[SimilarityTransform2D] = SimilarityTransform2D.identity() # input to global space transform
-    ) -> NDArray:
+        image: NDArray, 
+        centroid: Optional[NDArray] = None, # centroids in input space
+        ) -> Optional[Preprocessing]:
         
-        self.tracking_param.input_image_shape = image.shape
-                
-        preproc = preprocess_image(image, centroid, self.tracking_param)
+        return preprocess_image(image, centroid, self.tracking_param)
+
+    def track_resized(
+            self,
+            preproc: Preprocessing, 
+        ) -> Optional[Tuple[Tracking, NDArray]]:
         
-        if preproc is None:
-            return self.tracking_param.failed
-        
+        tracking = Tracking(num_animals=self.tracking_param.num_animals)
         mask = cv2.compare(preproc.image_processed, self.tracking_param.intensity, cv2.CMP_GT)
-        centroids_resized = bwareafilter_centroids_cv2(
+        tracking.centroids_resized = bwareafilter_centroids_cv2(
             mask, 
             min_size = self.tracking_param.min_size_px,
             max_size = self.tracking_param.max_size_px, 
@@ -35,22 +64,30 @@ class AnimalTracker_CPU(AnimalTracker):
             max_width = self.tracking_param.max_width_px
         )     
         
-        if centroids_resized.size == 0:
-            return self.tracking_param.failed
+        if tracking.centroids_resized.size == 0:
+            return None
+        
+        return tracking, mask
 
-        # transform coordinates
-        centroids_cropped = preproc.T_resized_to_cropped.transform_points(centroids_resized)
-        centroids_input = preproc.T_cropped_to_input.transform_points(centroids_cropped)
-        centroids_global = T_input_to_global.transform_points(centroids_input)
+    def transform_coordinate_system(
+            self,
+            tracking: Tracking,        
+            preproc: Preprocessing,
+            T_input_to_global,
+            T_global_to_input
+        ) -> AnimalResolution:
+        '''modifies tracking in-place with side-effect and returns resolution'''
+        
+        tracking.centroids_cropped = preproc.T_resized_to_cropped.transform_points(tracking.centroids_resized)
+        tracking.centroids_input = preproc.T_cropped_to_input.transform_points(tracking.centroids_cropped)
+        tracking.centroids_global = T_input_to_global.transform_points(tracking.centroids_input)
 
+        # TODO do the assignment in resized space instead !!!
         # identity assignment in global space
-        centroids_global = self.assignment.update(centroids_global)  
-
-        T_global_to_input = T_input_to_global.inv()
-
-        centroids_input = T_global_to_input.transform_points(centroids_global)
-        centroids_cropped = preproc.T_input_to_cropped.transform_points(centroids_input)
-        centroids_resized = preproc.T_cropped_to_resized.transform_points(centroids_cropped)
+        tracking.centroids_global = self.assignment.update(tracking.centroids_global)  
+        tracking.centroids_input = T_global_to_input.transform_points(tracking.centroids_global)
+        tracking.centroids_cropped = preproc.T_input_to_cropped.transform_points(tracking.centroids_input)
+        tracking.centroids_resized = preproc.T_cropped_to_resized.transform_points(tracking.centroids_cropped)
 
         # Downsample image export (a bit easier on RAM). This is used for overlay instead of image_cropped
         # NOTE: it introduces a special case, not a big fan of this
@@ -60,31 +97,64 @@ class AnimalTracker_CPU(AnimalTracker):
             cv2.INTER_NEAREST
         )
 
-        pix_per_mm_global = self.tracking_param.pix_per_mm
-        pix_per_mm_input = pix_per_mm_global * T_global_to_input.scale_factor
-        pix_per_mm_cropped = pix_per_mm_input * preproc.T_input_to_cropped.scale_factor
-        pix_per_mm_resized = pix_per_mm_cropped * preproc.T_cropped_to_resized.scale_factor
-        pix_per_mm_downsampled = pix_per_mm_input * self.tracking_param.downsample_factor
+        resolution = AnimalResolution()
+        resolution.pix_per_mm_global = self.tracking_param.pix_per_mm
+        resolution.pix_per_mm_input = resolution.pix_per_mm_global * T_global_to_input.scale_factor
+        resolution.pix_per_mm_cropped = resolution.pix_per_mm_input * preproc.T_input_to_cropped.scale_factor
+        resolution.pix_per_mm_resized = resolution.pix_per_mm_cropped * preproc.T_cropped_to_resized.scale_factor
+        resolution.pix_per_mm_downsampled = resolution.pix_per_mm_input * self.tracking_param.downsample_factor
 
+        return resolution, image_downsampled
+    
+    def track(
+        self,
+        image: Optional[NDArray], # image in input space
+        centroid: Optional[NDArray] = None, # centroid in global space
+        T_input_to_global: Optional[SimilarityTransform2D] = SimilarityTransform2D.identity() # input to global space transform
+    ) -> NDArray:
+        
+        self.tracking_param.input_image_shape = image.shape
+
+        centroid_in_input, T_global_to_input = self.transform_input_centroid(
+            centroid,
+            T_input_to_global
+        )
+
+        preproc = self.preprocess(image, centroid_in_input) 
+        if preproc is None:
+            return self.tracking_param.failed
+        
+        tracking_resized = self.track_resized(preproc)
+        if tracking_resized is None:
+            return self.tracking_param.failed
+
+        tracking, mask = tracking_resized
+        resolution, image_downsampled = self.transform_coordinate_system(
+            tracking, 
+            preproc, 
+            T_input_to_global, 
+            T_global_to_input
+        )
+        
         # NOTE: for large image/many fish, creating that array might take time
         # maybe pre-allocate in tracking_param?
         res = np.array(
             (
                 True,
                 self.tracking_param.num_animals,
-                centroids_resized,
-                centroids_cropped,
-                centroids_input,
-                centroids_global, 
+                tracking.centroids_resized,
+                tracking.centroids_cropped,
+                tracking.centroids_input,
+                tracking.centroids_global, 
                 self.tracking_param.downsample_factor,
                 mask, 
                 preproc.image_processed,
                 image_downsampled,
-                pix_per_mm_global,
-                pix_per_mm_input,
-                pix_per_mm_cropped,
-                pix_per_mm_resized,
-                pix_per_mm_downsampled
+                resolution.pix_per_mm_global,
+                resolution.pix_per_mm_input,
+                resolution.pix_per_mm_cropped,
+                resolution.pix_per_mm_resized,
+                resolution.pix_per_mm_downsampled
             ),
             dtype=self.tracking_param.dtype
         )
@@ -92,7 +162,6 @@ class AnimalTracker_CPU(AnimalTracker):
 
 
 class AnimalTrackerKalman(AnimalTracker_CPU):
-    # TODO take into account multiple animals
 
     def __init__(
             self, 
