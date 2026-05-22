0ba7b4516393d511a5c5b56012e44bc6e9def1b0
ElTinmar <martin.privat@hotmail.fr>
2026-05-22 21:47:35 +0200

    bump versions

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='tracker',
     python_requires='>=3.8',
     author='Martin Privat',
-    version='0.13.0',
+    version='0.13.1',
     packages=['tracker','tracker.animal','tracker.body','tracker.eyes','tracker.multifish','tracker.tail', 'tracker.singlefish'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='tracking zebrafish larvae',
@@ -16,8 +16,8 @@ setup(
         "tqdm",
         "numba",
         "filterpy",
-        "video_tools @ git+https://github.com/ElTinmar/video_tools.git@v0.6.6",
-        "image_tools @ git+https://github.com/ElTinmar/image_tools.git@v0.9.3",
+        "video_tools @ git+https://github.com/ElTinmar/video_tools.git@v0.6.7",
+        "image_tools @ git+https://github.com/ElTinmar/image_tools.git@v0.9.4",
         "geometry @ git+https://github.com/ElTinmar/geometry.git@v0.3.0",
         "kalman @ git+https://github.com/ElTinmar/Kalman.git@v0.1.7"
     ]
