e696dc1b0bd8863511b572d268ab57d269d93a20
ElTinmar <martin.privat@hotmail.fr>
2026-06-17 17:52:12 +0200

    transformn isnside predictor

--- tracker/head_embedded/tracker.py
+++ tracker/head_embedded/tracker.py
@@ -46,7 +46,7 @@ class HeadEmbeddedTracker_CPU(HeadEmbeddedTracker):
             T_image_rot_to_global
         )
 
-        skeleton_mm = (tail['skeleton_global'] - tail['skeleton_global'][0,:])/tail['pix_per_mm_global']
+        skeleton_mm = tail['skeleton_global']/tail['pix_per_mm_global']
         position = self.tracking_param.position_predictor.estimate(skeleton_mm)
         arr = (True, position.x, position.y, position.theta, tail)
         res = np.array(
