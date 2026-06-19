d384e64c19ad3cc38ecd94aa7f92d41997966a34
ElTinmar <martin.privat@hotmail.fr>
2026-06-19 15:39:07 +0200

    centroid translation

--- tracker/head_embedded/tracker.py
+++ tracker/head_embedded/tracker.py
@@ -42,7 +42,7 @@ class HeadEmbeddedTracker_CPU(HeadEmbeddedTracker):
         tail = self.tracking_param.tail.track(
             image_rot, 
             background_rot, 
-            centroid, 
+            np.array([self.tracking_param.centroid_x, self.tracking_param.centroid_y]), 
             T_image_rot_to_global
         )
 
