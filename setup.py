3fc568c0839617741a5c5d02780a7f36af0fd761
ElTinmar <martin.privat@hotmail.fr>
2026-06-23 11:52:38 +0200

    update dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='tracker',
     python_requires='>=3.8',
     author='Martin Privat',
-    version='0.14.4',
+    version='0.14.5',
     packages=['tracker','tracker.animal','tracker.body','tracker.eyes','tracker.multifish','tracker.tail', 'tracker.singlefish', 'tracker.head_embedded'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='tracking zebrafish larvae',
@@ -16,9 +16,9 @@ setup(
         "tqdm",
         "numba",
         "filterpy",
-        "video_tools @ git+https://github.com/ElTinmar/video_tools.git@v0.6.8",
-        "image_tools @ git+https://github.com/ElTinmar/image_tools.git@v0.9.4",
-        "geometry @ git+https://github.com/ElTinmar/geometry.git@v0.3.0",
+        "video_tools @ git+https://github.com/ElTinmar/video_tools.git@v0.6.9",
+        "image_tools @ git+https://github.com/ElTinmar/image_tools.git@v0.9.5",
+        "geometry @ git+https://github.com/ElTinmar/geometry.git@v0.3.1",
         "kalman @ git+https://github.com/ElTinmar/Kalman.git@v0.1.7"
     ]
 )
